72c024136c66eac44ffa0521d8f7c21cbf3ade76
Rachana1104 <y.rachana1104@gmail.com>
2021-12-15 18:08:31 +0530

    Mission grid made responsive

--- src/Components/HomePage/Mission/Mission.js
+++ src/Components/HomePage/Mission/Mission.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import OurMission from "./Mission_array";
 import "../HomePage.css";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 const Mission = () => {
   return (
@@ -8,7 +10,7 @@ const Mission = () => {
       <div className="ourMission">
         <h2>Our Mission</h2>
         <div className="ourMissionContainer">
-          {OurMission.map((mission) => {
+          {/* {OurMission.map((mission) => {
             if (mission.id < 4) {
               return (
                 <div className="mission">
@@ -28,7 +30,24 @@ const Mission = () => {
                 </div>
               );
             }
-          })}
+          })} */}
+          <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 3, sm: 6, md: 12 }}>
+          {OurMission.map((mission) => {
+            return(
+              
+              <div className="mission">
+                <Grid item xs={2} sm={4} md={4}>
+                <div className="missionImgContainer">
+                    <img src={mission.image} className="missionImg" />
+                  </div>
+                  <i className="missionDes">{mission.description}</i>
+                </Grid>
+              </div>
+            )
+  })}
+            </Grid>
+          </Box>
         </div>
       </div>
     </>
