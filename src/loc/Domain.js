06dc0af4e6120c7c1e4330aee2fc7c5d1ab73826
Khushi Mehta <khushimehta8282@gmail.com>
2022-01-09 17:06:36 +0530

    domain and timeline added

--- src/loc/Domain.js
+++ src/loc/Domain.js
@@ -1,9 +1,77 @@
+import { Card, Grid } from '@mui/material'
 import React from 'react'
+import './Loc.css'
+import { FaUserGraduate } from 'react-icons/fa'
+import { width } from '@mui/system'
 
 function Domain() {
     return (
         <div style={{ color: 'black', backgroundColor: 'white' }}>
-            Domain
+            <div className="domain" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', flexDirection: 'column' }}>
+                <h2 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', marginTop: '5%', marginBottom: '4%', color: '#3770FF' }} className='domainHeading'>Domain</h2>
+                <Grid container sx={{ width: '100%' }} style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', width: '100%', marginBottom: '5%', rowGap: '70px' }}>
+                    <Grid item xs={12} sm={6} md={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', flexDirection: 'column' }}>
+                        <div class="flip-card">
+                            <div class="flip-card-inner" >
+                                <div class="flip-card-front" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100%' }}>
+                                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                                        <FaUserGraduate style={{ color: 'white', width: '40px', height: '40px' }} />
+                                        <h3 style={{ color: 'white', margin: '0px', padding: '0px', marginTop: '10px' }}> AI/ML </h3>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100%' }}>
+                                    <p>Lorem ipsum dolor sit amet, consectur adipiscing elit. Feugiat porttitor duis aliquet arcu. Feugiat porttitor duis aliquet arcu. Feugiat porttior duis aliquet arcu. Feugiat porttitor duis aliquet arcu. Feugiat duis aliquet. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', flexDirection: 'column' }}>
+                        <div class="flip-card">
+                            <div class="flip-card-inner" >
+                                <div class="flip-card-front" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100%' }}>
+                                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                                        <FaUserGraduate style={{ color: 'white', width: '40px', height: '40px' }} />
+                                        <h3 style={{ color: 'white', margin: '0px', padding: '0px', marginTop: '10px' }}> AI/ML </h3>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100%' }}>
+                                    <p>Lorem ipsum dolor sit amet, consectur adipiscing elit. Feugiat porttitor duis aliquet arcu. Feugiat porttitor duis aliquet arcu. Feugiat porttior duis aliquet arcu. Feugiat porttitor duis aliquet arcu. Feugiat duis aliquet. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', flexDirection: 'column' }}>
+                        <div class="flip-card">
+                            <div class="flip-card-inner" >
+                                <div class="flip-card-front" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100%' }}>
+                                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                                        <FaUserGraduate style={{ color: 'white', width: '40px', height: '40px' }} />
+                                        <h3 style={{ color: 'white', margin: '0px', padding: '0px', marginTop: '10px' }}> AI/ML </h3>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100%' }}>
+                                    <p>Lorem ipsum dolor sit amet, consectur adipiscing elit. Feugiat porttitor duis aliquet arcu. Feugiat porttitor duis aliquet arcu. Feugiat porttior duis aliquet arcu. Feugiat porttitor duis aliquet arcu. Feugiat duis aliquet. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', flexDirection: 'column' }}>
+                        <div class="flip-card">
+                            <div class="flip-card-inner" >
+                                <div class="flip-card-front" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100%' }}>
+                                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                                        <FaUserGraduate style={{ color: 'white', width: '40px', height: '40px' }} />
+                                        <h3 style={{ color: 'white', margin: '0px', padding: '0px', marginTop: '10px' }}> AI/ML </h3>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100%' }}>
+                                    <p>Lorem ipsum dolor sit amet, consectur adipiscing elit. Feugiat porttitor duis aliquet arcu. Feugiat porttitor duis aliquet arcu. Feugiat porttior duis aliquet arcu. Feugiat porttitor duis aliquet arcu. Feugiat duis aliquet. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
--- src/loc/Timeline.js
+++ src/loc/Timeline.js
@@ -1,9 +1,65 @@
+import { Grid } from '@mui/material'
 import React from 'react'
+import './Loc.css'
 
 function Timeline() {
     return (
         <div style={{ backgroundColor: '#001521' }}>
-            Timeline
+            <div className="timeline" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', flexDirection: 'column', marginBottom: '5%' }}>
+                <h2 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', marginTop: '5%', marginBottom: '4%' }} className='prizesHeading'>Timeline</h2>
+                <div className='timeStamps' >
+                    <div style={{ width: '100%', marginTop: '15px' }}>
+                        <Grid container className='timelineGrid'  >
+                            <Grid item md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                <div style={{ padding: '2px', backgroundColor: '#3770FF', borderRadius: '50%', width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>1</div>
+                            </Grid>
+                            <Grid item md={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><p>8th Feb</p></Grid>
+                            <Grid item md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><p> - </p></Grid>
+                            <Grid item md={5} ><p>Registration Begins</p></Grid>
+                        </Grid>
+                    </div>
+                    <div style={{ width: '100%', marginTop: '15px' }}>
+                        <Grid container className='timelineGrid' >
+                            <Grid item md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                <div style={{ padding: '2px', backgroundColor: '#3770FF', borderRadius: '50%', width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>2</div>
+                            </Grid>
+                            <Grid item md={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><p>8th Feb</p></Grid>
+                            <Grid item md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><p> - </p></Grid>
+                            <Grid item md={5} ><p>Registration Ends</p></Grid>
+                        </Grid>
+                    </div>
+                    <div style={{ width: '100%', marginTop: '15px' }}>
+                        <Grid container className='timelineGrid' >
+                            <Grid item md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                <div style={{ padding: '2px', backgroundColor: '#3770FF', borderRadius: '50%', width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>3</div>
+                            </Grid>
+                            <Grid item md={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><p>8th Feb</p></Grid>
+                            <Grid item md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><p> - </p></Grid>
+                            <Grid item md={5} ><p>Shortlisted Teams Out</p></Grid>
+                        </Grid>
+                    </div>
+                    <div style={{ width: '100%', marginTop: '15px' }}>
+                        <Grid container className='timelineGrid' >
+                            <Grid item md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                <div style={{ padding: '2px', backgroundColor: '#3770FF', borderRadius: '50%', width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>4</div>
+                            </Grid>
+                            <Grid item md={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><p>8th Feb</p></Grid>
+                            <Grid item md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><p> - </p></Grid>
+                            <Grid item md={5} ><p>Hack Day</p></Grid>
+                        </Grid>
+                    </div>
+                    <div style={{ width: '100%', marginTop: '15px' }}>
+                        <Grid container className='timelineGrid' >
+                            <Grid item md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                <div style={{ padding: '2px', backgroundColor: '#3770FF', borderRadius: '50%', width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>5</div>
+                            </Grid>
+                            <Grid item md={3} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><p>8th Feb</p></Grid>
+                            <Grid item md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><p> - </p></Grid>
+                            <Grid item md={5} ><p>Price Distribution</p></Grid>
+                        </Grid>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
