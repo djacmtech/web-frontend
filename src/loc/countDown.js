a574491a17d1c7e378c2e2d5521cdb330092e03b
Khushi Mehta <khushimehta8282@gmail.com>
2022-01-17 10:12:42 +0530

    made countdown responsive

--- src/loc/countDown.js
+++ src/loc/countDown.js
@@ -1,58 +1,49 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Grid from '@mui/material/Grid'
+import './Loc.css'
 
 const CountDown = () => {
 
   const calculateTimeLeft = () => {
     let diff = +new Date("February 27, 2022 00:00:01").getTime() - +new Date().getTime()
     let timeLeft = {
-      DAYS: Math.floor((diff/(1000*60*60*24))),
-      HOURS: Math.floor((diff/(1000*60*60))%24),
-      MINUTES: Math.floor((diff/1000/60)%60),
-      SECONDS: Math.floor((diff/1000)%60)
+      DAYS: Math.floor((diff / (1000 * 60 * 60 * 24))),
+      HOURS: Math.floor((diff / (1000 * 60 * 60)) % 24),
+      MINUTES: Math.floor((diff / 1000 / 60) % 60),
+      SECONDS: Math.floor((diff / 1000) % 60)
     }
     return timeLeft
   }
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
 
-  useEffect(()=>{
-    const timer = setTimeout(()=>{
+  useEffect(() => {
+    const timer = setTimeout(() => {
       setTimeLeft((calculateTimeLeft()))
     }, 1000)
-    return ()=> clearTimeout(timer)
+    return () => clearTimeout(timer)
   })
 
   const timerComponents = []
 
-  Object.keys(timeLeft).forEach((interval, index)=>{
-    if(!timeLeft[interval]) {
+  Object.keys(timeLeft).forEach((interval, index) => {
+    if (!timeLeft[interval]) {
       return
     }
-    if(index<=2) {
-      timerComponents.push(
-        <Grid item md={3} sm={3} xs={3} style={{padding:'20px 10px 20px 10px'}}>
-          <span style={{fontSize:'3.3rem', fontWeight:'bolder'}}>{timeLeft[interval]} : </span><br/>
-          <span style={{fontSize:'0.8rem', fontWeight:'600'}}> {interval} </span> 
-        </Grid> 
-      )
-    }
-    else {
-      timerComponents.push(
-        <Grid item md={3} sm={3} xs={3} style={{padding:'20px 10px 20px 10px'}}>
-          <span style={{fontSize:'3.3rem', fontWeight:'bolder'}}>{timeLeft[interval]} </span><br/>
-          <span style={{fontSize:'0.8rem', fontWeight:'600'}}> {interval} </span> 
-        </Grid> 
-      )
-    }
+    timerComponents.push(
+      <Grid item md={3} sm={3} xs={3} style={{ padding: '20px 10px 20px 10px' }}>
+        <span style={{ fontWeight: 'bolder' }} className="countDownSize">{timeLeft[interval]}{index - 3 ? ' : ' : ''}</span> <br />
+        <span style={{ fontWeight: '600' }} className="countDownIntervalSize"> {interval} </span>
+      </Grid>
+    )
   })
 
   return (
-    <div style={{padding:'50px'}}>
-        <Grid container spacing={2} style={{width:'45%', fontFamily:'montserrat', border:'3px solid #FF8A50'}}>
-          {timerComponents.length? timerComponents : <span> Time's up! </span>}
-        </Grid>
-        <br/>
+    <div style={{ padding: '50px' }}>
+      <Grid container spacing={2} style={{ fontFamily: 'montserrat', border: '3px solid #FF8A50' }} className="countdownBorder">
+        {timerComponents.length ? timerComponents : <span> Time's up! </span>}
+      </Grid>
+      <br />
     </div>
   );
 };
