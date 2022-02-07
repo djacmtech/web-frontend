930edfc4a7422ec34c3635007aef0fb5a36dc368
Khushi Mehta <khushimehta8282@gmail.com>
2022-02-07 18:19:53 +0530

    countdown + timeline responsive

--- src/loc/countDown.js
+++ src/loc/countDown.js
@@ -7,10 +7,10 @@ const CountDown = () => {
   const calculateTimeLeft = () => {
     let diff = +new Date("February 27, 2022 00:00:01").getTime() - +new Date().getTime()
     let timeLeft = {
-      DAYS: Math.floor((diff / (1000 * 60 * 60 * 24)))/10<1? '0'+ Math.floor((diff / (1000 * 60 * 60 * 24))): Math.floor((diff / (1000 * 60 * 60 * 24))),
-      HOURS: Math.floor((diff / (1000 * 60 * 60)) % 24)/10<1? '0'+Math.floor((diff / (1000 * 60 * 60)) % 24): Math.floor((diff / (1000 * 60 * 60)) % 24),
-      MINUTES: Math.floor((diff / 1000 / 60) % 60)/10<1? '0'+Math.floor((diff / 1000 / 60) % 60): Math.floor((diff / 1000 / 60) % 60),
-      SECONDS: Math.floor((diff / 1000) % 60)/10<1? '0'+Math.floor((diff / 1000) % 60): Math.floor((diff / 1000) % 60)
+      DAYS: Math.floor((diff / (1000 * 60 * 60 * 24))) / 10 < 1 ? '0' + Math.floor((diff / (1000 * 60 * 60 * 24))) : Math.floor((diff / (1000 * 60 * 60 * 24))),
+      HOURS: Math.floor((diff / (1000 * 60 * 60)) % 24) / 10 < 1 ? '0' + Math.floor((diff / (1000 * 60 * 60)) % 24) : Math.floor((diff / (1000 * 60 * 60)) % 24),
+      MINUTES: Math.floor((diff / 1000 / 60) % 60) / 10 < 1 ? '0' + Math.floor((diff / 1000 / 60) % 60) : Math.floor((diff / 1000 / 60) % 60),
+      SECONDS: Math.floor((diff / 1000) % 60) / 10 < 1 ? '0' + Math.floor((diff / 1000) % 60) : Math.floor((diff / 1000) % 60)
     }
     return timeLeft
   }
@@ -39,7 +39,7 @@ const CountDown = () => {
   })
 
   return (
-    <div style={{ padding: '50px' }}>
+    <div className='countdownSection'>
       <Grid container spacing={2} style={{ fontFamily: 'montserrat', border: '3px solid #FF8A50' }} className="countdownBorder">
         {timerComponents.length ? timerComponents : <span> Time's up! </span>}
       </Grid>
