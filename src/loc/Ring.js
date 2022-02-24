dd93d1f02479dc4229f86371cad608c44c85eff5
Deap Daru <deapdaru@gmail.com>
2022-02-24 21:50:07 +0530

    Torus and Cylinder animation update

--- src/loc/Ring.js
+++ src/loc/Ring.js
@@ -1,19 +1,19 @@
-import { useRef } from "react";
-import { useFrame } from "@react-three/fiber"
+import {useRef} from "react";
+import {useFrame} from "@react-three/fiber";
 
 export default function Ring() {
+  const myMesh = useRef();
 
-    const myMesh = useRef();
+  useFrame(({clock}) => {
+    const a = clock.getElapsedTime();
+    myMesh.current.rotation.x = a * 0.4;
+    myMesh.current.position.y = Math.cos(a * 0.3);
+  });
 
-    useFrame(({ clock }) => {
-        const a = clock.getElapsedTime();
-        myMesh.current.rotation.x = a*0.4;
-      });
-
-    return (
-        <mesh rotation={[-0.3, 28.9, 79.2]} ref={myMesh}>
-            <torusBufferGeometry attach='geometry' args={[1.5, 0.3, 16, 100, 10]}/>
-            <meshNormalMaterial attach="material"/>
-        </mesh>
-    )
+  return (
+    <mesh rotation={[-0.3, 28.9, 79.2]} ref={myMesh}>
+      <torusBufferGeometry attach="geometry" args={[1.5, 0.3, 16, 100, 10]} />
+      <meshNormalMaterial attach="material" />
+    </mesh>
+  );
 }
