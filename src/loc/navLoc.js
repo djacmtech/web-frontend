015d1f63c6f3d1ad415d5e37a09d68b846ffc6a6
Bhumika Mange <bhumikamange13@gmail.com>
2022-02-24 15:14:38 +0530

    elevation added to navbar

--- src/loc/navLoc.js
+++ src/loc/navLoc.js
@@ -12,6 +12,7 @@ import { Typography } from "@mui/material";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import { Link } from "react-scroll";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
 const pages = [
   "Prizes",
   "Domain",
@@ -22,9 +23,23 @@ const pages = [
   "Contact Us",
 ];
 
-const NavLoc = (elev) => {
-  console.log(elev.elevation);
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
 
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const NavLoc = (elev) => {
   const [state, setState] = React.useState({
     top: false,
     left: false,
@@ -72,65 +87,72 @@ const NavLoc = (elev) => {
   );
 
   return (
-    <AppBar position="sticky" elevation={elev.elevation} style={{backgroundColor:'#001521'}}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
-          >
-            <img src={logo} alt="logo" width="50" ></img>
-          </Typography>
+    <ElevationScroll>
+      <AppBar position="sticky" style={{ backgroundColor: "#001521" }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+            >
+              <img src={logo} alt="logo" width="50"></img>
+            </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <React.Fragment key={anchor}>
-              <Button
-                style={{ fontSize: "2rem" }}
-                size="large"
-                color="inherit"
-                onClick={toggleDrawer(anchor, true)}
-              >
-                <GiHamburgerMenu />
-              </Button>
-              <Drawer
-                anchor={anchor}
-                open={state[anchor]}
-                onClose={toggleDrawer(anchor, false)}
-              >
-                {list(anchor)}
-              </Drawer>
-            </React.Fragment>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}
-          >
-            <img src={logo} alt="logo" width="50"></img>
-          </Typography>
-          <Box
-            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
-            justifyContent="center"
-            alignContent="center"
-          >
-            {pages.map((page, index) => (
-              <Link key={index} to={page} spy={true} smooth={true}>
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <React.Fragment key={anchor}>
                 <Button
-                  key={page}
-                  style={{fontSize:'1.2rem'}}
-                  sx={{ my: 2, color: "white", display: "block", paddingInline:'15px' }}
+                  style={{ fontSize: "2rem" }}
+                  size="large"
+                  color="inherit"
+                  onClick={toggleDrawer(anchor, true)}
                 >
-                  {page}
+                  <GiHamburgerMenu />
                 </Button>
-              </Link>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+                <Drawer
+                  anchor={anchor}
+                  open={state[anchor]}
+                  onClose={toggleDrawer(anchor, false)}
+                >
+                  {list(anchor)}
+                </Drawer>
+              </React.Fragment>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}
+            >
+              <img src={logo} alt="logo" width="50"></img>
+            </Typography>
+            <Box
+              sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+              justifyContent="center"
+              alignContent="center"
+            >
+              {pages.map((page, index) => (
+                <Link key={index} to={page} spy={true} smooth={true}>
+                  <Button
+                    key={page}
+                    style={{ fontSize: "1.2rem" }}
+                    sx={{
+                      my: 2,
+                      color: "white",
+                      display: "block",
+                      paddingInline: "15px",
+                    }}
+                  >
+                    {page}
+                  </Button>
+                </Link>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ElevationScroll>
   );
 };
 export default NavLoc;
