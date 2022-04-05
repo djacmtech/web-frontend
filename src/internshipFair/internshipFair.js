2fdf11059770567e31c85b17db5ffd6f284290f6
Rachana1104 <y.rachana1104@gmail.com>
2022-04-05 21:29:23 +0530

    features page added

--- src/internshipFair/internshipFair.js
+++ src/internshipFair/internshipFair.js
@@ -3,12 +3,14 @@ import HeroFair from "./HeroFair";
 import NavbarFair from "./NavbarFair";
 import AboutFair from "./AboutFair";
 import "./InternshipFair.css";
+import FeaturesFair from "./FeaturesFair";
 const InternshipFair = () => {
   return (
     <div style={{ backgroundColor: "white" }}>
       <NavbarFair />
       <HeroFair />
       <AboutFair/>
+      <FeaturesFair/>
     </div>
   );
 };
