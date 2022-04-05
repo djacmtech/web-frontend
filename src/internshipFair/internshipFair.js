a4b2f1d63eacc8d7f9c28cf71f7dc091716dde0b
Rachana1104 <y.rachana1104@gmail.com>
2022-04-06 00:39:14 +0530

    past companies page added

--- src/internshipFair/internshipFair.js
+++ src/internshipFair/internshipFair.js
@@ -5,12 +5,14 @@ import AboutFair from "./AboutFair";
 import "./InternshipFair.css";
 import FeaturesFair from "./FeaturesFair";
 import FairFooter from "./FairFooter";
+import PastCompanies from "./PastCompanies";
 const InternshipFair = () => {
   return (
     <div style={{ backgroundColor: "white", transform: 'translateY(-5px)' }}>
       <NavbarFair />
       <HeroFair />
       <AboutFair/>
+      <PastCompanies/>
       <FeaturesFair/>
       <FairFooter />
     </div>
