c5a9ce33559609e90713d86a150c8d94383daac2
talkshrey <talkshrey@gmail.com>
2022-04-30 19:42:30 +0530

    Dashboard alignment changed

--- src/internshipFair/Dashboard.js
+++ src/internshipFair/Dashboard.js
@@ -31,11 +31,11 @@ export default function Dashboard () {
         <div>
             <InterNavbar/>
             <Grid container spacing={0} style={{padding:'10%', backgroundColor:'#E5E5E5'}}>
-                <Grid md={6} style={{paddingInline:'3%'}}> 
+                <Grid xs={12} sm={12} md={6} style={{paddingInline:'3%'}}> 
                     <p style={{paddingInline:'10%', backgroundColor:'#E5E5E5', color:'#2D3748', fontSize:'2rem', fontWeight:'bold'}}> Student Dashboard </p>
                     <Filter/>
                 </Grid>
-                <Grid item md={6}>
+                <Grid item xs={12} sm={12} md={6}>
                     {
                         job.map((items)=>
                             <JobCard 
